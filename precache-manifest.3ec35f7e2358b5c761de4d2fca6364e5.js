0286c950b2a9b54eb6ce26ebbe07f8976f1dbe60
Sveta <1985vet@gmail.com>
2020-04-02 20:04:06 +0300

    Updates

---
+++ precache-manifest.3ec35f7e2358b5c761de4d2fca6364e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a41ef7151e660e2eb912daeab12c1f3f",
+    "url": "/movies/index.html"
+  },
+  {
+    "revision": "1e55d523c8005b1f1deb",
+    "url": "/movies/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "41a10328d4919d7de867",
+    "url": "/movies/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1e55d523c8005b1f1deb",
+    "url": "/movies/static/js/2.bb54a35c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/movies/static/js/2.bb54a35c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41a10328d4919d7de867",
+    "url": "/movies/static/js/main.3897141d.chunk.js"
+  },
+  {
+    "revision": "62d4ffbc6324dd0b8763",
+    "url": "/movies/static/js/runtime-main.01225fbc.js"
+  }
+]);
