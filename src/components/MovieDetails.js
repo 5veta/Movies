7e3a20a9f8dce39d002ae98f6d5361670cb8893e
5veta <5veta@users.noreply.github.com>
2020-06-13 13:30:32 +0300

    MovieDetails is changed on statles conponent and added Hooks

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,83 +1,74 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { API_KEY_3 } from "../utils/api";
 import MovieInfo from "./MovieInfo";
 import Cast from "./Cast";
 import Video from "./Video";
 
-class MovieDetails extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      movie: { id: props.match.params.id },
-      credits: {},
-      video: {}
-    };
-  }
+const MovieDetails = props => {
+  const [movie, setMovie] = useState({ id: props.match.params.id });
+  const [credits, setCredits] = useState({});
+  const [video, setVideo] = useState({});
 
-  componentDidMount() {
-    this.getMovieDetails();
-  }
-
-  getMovieDetails = () => {
-    fetch(
-      `https://api.themoviedb.org/3/movie/${
-        this.state.movie.id
-      }?api_key=${API_KEY_3}`
-    )
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${movie.id}?api_key=${API_KEY_3}`)
       .then(response => response.json())
       .then(data => {
-        this.setState({ movie: data });
+        setMovie(data);
       })
       .catch(err =>
         console.error("Error getting details information", err.stack)
       );
+  }, [movie]);
+
+  useEffect(() => {
     fetch(
       `https://api.themoviedb.org/3/movie/${
-        this.state.movie.id
+        movie.id
       }/credits?api_key=${API_KEY_3}`
     )
       .then(response => response.json())
       .then(data => {
-        this.setState({ credits: data });
+        setCredits(data);
       })
       .catch(err =>
-        console.error("Error getting details information", err.stack)
+        console.error("Error getting credits information", err.stack)
       );
+  }, [credits]);
+
+  useEffect(() => {
     fetch(
       `https://api.themoviedb.org/3/movie/${
-        this.state.movie.id
+        movie.id
       }/videos?api_key=${API_KEY_3}&language=en-US`
     )
       .then(response => response.json())
       .then(data => {
-        this.setState({ video: data.results[0] });
+        setVideo(data.results[0]);
       })
       .catch(err =>
-        console.error("Error getting details information", err.stack)
+        console.error("Error getting video information", err.stack)
       );
-  };
+  }, [video]);
 
-  render() {
-    return (
-      <div>
-        <div className="d-flex flex-row justify-content-center bg-info my-4 text-white">
-          <div className="card m-4 p-3 col-md-8 border-0 bg-info ">
-            <MovieInfo movie={this.state.movie} />
-          </div>
-        </div>
-        <div className="d-flex flex-row justify-content-center my-4 ">
-          <div className="col-md-10 scroll">
-            <Cast credits={this.state.credits} />
-          </div>
-        </div>
-        <div className="d-flex flex-row justify-content-center my-4 ">
-          <div className="col-8">
-            <Video video={this.state.video} />
-          </div>
+  return (
+    <div>
+      <div className="d-flex flex-row justify-content-center bg-info my-4 text-white">
+        <div className="card m-4 p-3 col-md-8 border-0 bg-info ">
+          <MovieInfo movie={movie} />
         </div>
       </div>
-    );
-  }
-}
+      <div className="d-flex flex-row justify-content-center my-4 ">
+        <div className="col-md-10 scroll">
+          <Cast credits={credits} />
+        </div>
+      </div>
+      <div className="d-flex flex-row justify-content-center my-4 ">
+        <div className="col-8">
+          <Video video={video} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default MovieDetails;
